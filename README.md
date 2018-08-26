fa10cc1cfd7b52fe9a7989d9e7a66d5c3f81b6a8
Eric McConkey <daryth@conquer.local>
2018-08-26 11:59:23 -0600

    Added example script and first revision of download script

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# new_vm_docker_setup
+# New VM Docker Setup
+
+Run this command via:
+curl -s -o run.sh https://raw.githubusercontent.com/emcconkey/new_vm_docker_setup/master/setup_docker.sh && source run.sh
+
--- setup_docker.sh
+++ setup_docker.sh
@@ -1,2 +1,6 @@
-echo "first test command"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update -y
+sudo apt-get install -y docker-ce
+sudo usermod -aG docker ubuntu
 
